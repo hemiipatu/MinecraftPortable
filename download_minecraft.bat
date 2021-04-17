147074e9fc8907704851846b753f85c20c86218b
hemiipatu <16671116+hemiipatu@users.noreply.github.com>
2021-04-18 00:25:30 +0100

    mkdir && download minecraft.exe.

--- download_minecraft.bat
+++ download_minecraft.bat
@@ -6,3 +6,14 @@ color 02
 :: Purpose: A batch script to download the official Minecraft.exe, Java JDK and format folders properly.
 :: Author: https://github.com/hemiipatu
 :: Revision/Commit History: https://github.com/hemiipatu/MinecraftPortable/commits/master
+
+:: Create directories/folders required to run Minecraft as a portable application.
+mkdir bin
+mkdir cache
+mkdir data
+
+:: Change directory to 'bin'.
+cd bin
+
+:: Download Minecraft.exe and change the name to 'MinecraftLauncher.exe'.
+wget https://launcher.mojang.com/download/Minecraft.exe -O MinecraftLauncher.exe
