ad2b8122d35b16011a5a976edafaaeb1988a80c4
hemiipatu <16671116+hemiipatu@users.noreply.github.com>
2021-04-17 23:54:59 +0100

    dequote.

--- start_minecraft.bat
+++ start_minecraft.bat
@@ -26,3 +26,6 @@ set path=%startdir%\bin\runtime\jdk-15.0.2\bin
 
 :start
 start "" "%startdir%\bin\MinecraftLauncher.exe" --workDir "%workdir%" --lockDir  "%lockdir%"
+
+:deQuote
+for /f "delims=" %%A in ('echo %%%1%%') do set %1=%%~A
