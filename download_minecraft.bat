09ff9fdd610d2a4c3d9abee6a523ac8c7e08c960
hemiipatu <16671116+hemiipatu@users.noreply.github.com>
2021-04-18 00:05:16 +0100

    create download_minecraft.bat

---
+++ download_minecraft.bat
@@ -0,0 +1,8 @@
+@echo off
+title Minecraft 'portable' downloader.
+color 02
+
+:: Name: download_minecraft.bat
+:: Purpose: A batch script to download the official Minecraft.exe, Java JDK and format folders properly.
+:: Author: https://github.com/hemiipatu
+:: Revision/Commit History: https://github.com/hemiipatu/MinecraftPortable/commits/master
