3656060a1b08a3b1cc6f880c66947648c200fd03
hemiipatu <16671116+hemiipatu@users.noreply.github.com>
2021-04-17 21:42:49 +0100

    included link to commit history.

--- start_minecraft.bat
+++ start_minecraft.bat
@@ -5,4 +5,4 @@ color 02
 :: Name: start_minecraft.bata
 :: Purpose: A batch script that allows Minecraft to be run portable.
 :: Author: https://github.com/hemiipatu
-:: Revision History: 
+:: Revision/Commit History: https://github.com/hemiipatu/MinecraftPortable/commits/master
