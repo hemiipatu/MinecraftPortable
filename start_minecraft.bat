d8f4f2791f70d8f1885f04bfeca9f71c001d68b1
Hemi <16671116+hemiipatu@users.noreply.github.com>
2025-11-13 12:50:01 +0000

    Update start_minecraft.bat
    Made the code more efficient.

--- start_minecraft.bat
+++ start_minecraft.bat
@@ -2,50 +2,87 @@
 title Minecraft 'portable' launcher.
 color 02
 
-:: Name:                        start_minecraft.bat
-:: Purpose:                     A batch script that allows Minecraft to be run portable.
-:: Author:                      https://github.com/hemiipatu
-:: Revision/Commit History:     https://github.com/hemiipatu/MinecraftPortable/commits/master
+:: Name:            start_minecraft.bat
+:: Purpose:         A batch script that allows Minecraft to be run portable.
+:: Author:          https://github.com/hemiipatu
+:: Revision/Commit History: https://github.com/hemiipatu/MinecraftPortable/commits/master
 
-:setLocationRootDir
-set location=%~dp0
-set locationroot=%location%
-IF "%location:~-1%" == "\" (
-    set "locationroot=%location:~0,-1%" && goto setJavaDir
-) ELSE goto setJavaDir
+:: --- Configuration ---
+set "verljava=jdk-19.0.2"
+set "versjava=19"
+set "launcher_url=https://launcher.mojang.com/download/Minecraft.exe"
+set "java_url=https://download.oracle.com/java/%versjava%/archive/%verljava%_windows-x64_bin.zip"
 
-:setJavaDir
-set versjava=19
-set verljava=jdk-19.0.2
-set locationjava="%locationroot%"\bin\runtime\%verljava%\bin
-    goto setCurl
+:: Get the directory of the script and define root paths
+:: %~dp0 is the drive and path, the / separator is usually included.
+set "locationroot=%~dp0"
+IF "%locationroot:~-1%" == "\" (
+    set "locationroot=%locationroot:~0,-1%"
+)
+set "bindir=%locationroot%\bin"
+set "runtimepath=%bindir%\runtime"
+set "launcherpath=%bindir%\MinecraftLauncher.exe"
+set "java_zip_path=%runtimepath%\%verljava%.zip"
+set "java_exe_path=%runtimepath%\%verljava%\bin\java.exe"
+set "datadir=%locationroot%\data"
 
-:setCurl
-set curl=%systemroot%\system32\curl.exe
-    goto setTar
+:: --- System Tools ---
+:: Curl and Tar are available in modern Windows 10/11 (version 17063+)
+set "curl=%systemroot%\system32\curl.exe"
+set "tar=%systemroot%\system32\tar.exe"
 
-:setTar
-set tar=%systemroot%\system32\tar.exe
-    goto checkLauncherPrerequisites
+:: --- Check and Install Launcher ---
+echo Checking for Minecraft Launcher...
+IF NOT exist "%launcherpath%" (
+    echo Launcher not found. Creating directories and downloading...
+    
+    :: Use 'if not exist' to prevent errors if dir is already made
+    IF NOT exist "%bindir%" mkdir "%bindir%"
+    IF NOT exist "%runtimepath%" mkdir "%runtimepath%"
+    IF NOT exist "%datadir%" mkdir "%datadir%"
+    
+    :: Download Launcher
+    "%curl%" "%launcher_url%" -o "%launcherpath%"
+    
+    IF NOT exist "%launcherpath%" (
+        echo ERROR: Failed to download Minecraft Launcher. Exiting.
+        pause
+        exit /b 1
+    )
+)
 
-:: Begin the process of checking prerequisites.
-:checkLauncherPrerequisites
-IF NOT exist "%locationroot%"\bin\MinecraftLauncher.exe (
-    mkdir "%locationroot%"\bin "%locationroot%"\bin\runtime "%locationroot%"\cache "%locationroot%"\data && %curl% https://launcher.mojang.com/download/Minecraft.exe > "%locationroot%"/bin/MinecraftLauncher.exe
-) ELSE goto checkJavaPrerequisites
+:: --- Check and Install Java Runtime ---
+echo Checking for Java runtime...
+IF NOT exist "%java_exe_path%" (
+    echo Java runtime not found. Downloading and unzipping...
+    
+    :: Download Java
+    "%curl%" -H "Cookie: oraclelicense=accept-securebackup-cookie" "%java_url%" -o "%java_zip_path%"
+    
+    IF NOT exist "%java_zip_path%" (
+        echo ERROR: Failed to download Java Runtime. Exiting.
+        pause
+        exit /b 1
+    )
+    
+    :: Unzip Java
+    echo Unzipping Java...
+    :: -C extracts to a directory, and the file contents will create the %verljava% folder inside
+    "%tar%" -xvf "%java_zip_path%" -C "%runtimepath%"
+    
+    :: Clean up zip file
+    del "%java_zip_path%"
+    
+    IF NOT exist "%java_exe_path%" (
+        echo ERROR: Failed to extract Java Runtime. Exiting.
+        pause
+        exit /b 1
+    )
+)
 
-:checkJavaPrerequisites
-IF NOT exist %locationjava%\java.exe (
-    %curl% -H "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/%versjava%/archive/%verljava%_windows-x64_bin.zip > "%locationroot%"/bin/runtime/%verljava%.zip && goto unzip
-) ELSE goto start
+:: --- Start Minecraft ---
+echo Starting Minecraft Launcher...
+start "" "%launcherpath%" --workDir "%datadir%" --lockDir "%datadir%\.minecraft"
 
-:unzip
-%tar% -xvf "%locationroot%"/bin/runtime/%verljava%.zip -C "%locationroot%"/bin/runtime/
-goto start
-
-:start
-start "" "%locationroot%\bin\MinecraftLauncher.exe" --workDir "%locationroot%\data" --lockDir  "%locationroot%\data\.minecraft"
-goto end
-
-:end
-exit
+:: --- End Script ---
+exit /b 0
